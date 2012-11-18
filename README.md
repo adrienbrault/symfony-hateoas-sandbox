d70101766db85408c96ba0747cceadec9c679b4f
Adrien Brault <adrien.brault@gmail.com>
2012-11-18 16:34:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony Hateoas Edition
+=======================
+
+Symfony 2 application meant to be an example on how to build an HATEOAS api.
---
+++ src/AdrienBrault/ApiBundle/AdrienBraultApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AdrienBrault\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdrienBraultApiBundle extends Bundle
+{
+
+}
